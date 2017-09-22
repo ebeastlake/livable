b393bfff9016f3be790e9acab1dfb21826c9c354
ebeastlake <ebeastlake@gmail.com>
2017-09-22 11:17:55 -0400

    feat: adding places returned from Places API to state (basic)

--- client/reducer/places.js
+++ client/reducer/places.js
@@ -4,21 +4,26 @@ import axios from 'axios';
 const FIND_PLACES = 'FIND_PLACES';
 
 // action creator
-export function findPlaces() {
+export function findPlaces(places) {
 	return {type: FIND_PLACES, places: places};
 }
 
 // thunk creator
 export function queryAPI(query) {
 	return function thunk(dispatch) {
-		return axios.get(`/api/places/${query}`);
+		console.log('in thunk')
+		return axios.get(`/api/places/${query}`)
+		.then(res => res.data)
+		.then(places => {
+			dispatch(findPlaces(places));
+		})
 	};
 }
 
 function placesReducer(state = [], action) {
 	switch (action.type) {
 		case FIND_PLACES: {
-			return state;
+			return state.concat(action.places);
 		}
 		default: {
 			return state;
